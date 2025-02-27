da03815534b53b2030e9dbf88fccf1aec52e145e
s2cjoseph <j.roxas@set2close.io>
2025-02-28 06:07:38 +0800

    Add Homepage Route

--- index.js
+++ index.js
@@ -12,5 +12,23 @@ require('dotenv').config()
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_KEY;
 
 
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+
+app.get('/', async (req, res) => {
+    const books = `https://api.hubspot.com/crm/v3/objects/books?limit=10&properties=name,author,genre&archived=false`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(books, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Homepage Books | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
